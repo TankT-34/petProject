b9531c9804de759cbf96be7cc4d65421c5f056b5
Татьяна <9a_borisova@mail.ru>
2026-02-13 22:10:20 +0300

    Добавленные изменения

--- src/main/java/com/bookstore/service/BookServiceImpl.java
+++ src/main/java/com/bookstore/service/BookServiceImpl.java
@@ -14,6 +14,10 @@ public class BookServiceImpl implements BookService {
 
     private final BookRepository bookRepository;  // ← final поле, без @Autowired
 
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
     @Override
     public Book getBookById(Long id) {
         return bookRepository.findById(id)
